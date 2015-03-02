63e16c1c6c98749ffdf0174c35c5a698bb0f524e
nguyentrungdev <nguyentrungdev@gmail.com>
2015-03-02 10:25:29 +0700

    first commit

---
+++ src/main/java/com/aavn/primefaces/UserLoginView.java
@@ -0,0 +1,33 @@
+package com.aavn.primefaces;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+@SessionScoped
+public class UserLoginView {
+	private String username;
+    
+    private String password;
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    public void login(ActionEvent event) {
+        
+    }   
+}
